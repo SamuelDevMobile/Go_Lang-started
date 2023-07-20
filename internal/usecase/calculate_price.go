a9631ff0a04693b49ee3b6496cd79b04a98d41bf
Samuel Ribeiro <samuelribeiro9@icloud.com>
2023-07-20 00:22:26 -0300

    criado primeiro caso de uso

---
+++ internal/usecase/calculate_price.go
@@ -0,0 +1,42 @@
+package usecase
+
+import "github.com/SamuelDevMobile/Go_Lang-started/internal/entitys"
+
+type OrderInput struct {
+	ID    string
+	Price float64
+	Tax   float64
+}
+
+type OrderOutput struct {
+	ID         string
+	Price      float64
+	Tax        float64
+	FinalPrice float64
+}
+
+// SOLID - "D" - Dependency Inversion Principle
+type CalculateFinalPrice struct {
+	OrderRepository entitys.OrderRepositoryInterface
+}
+
+func (c *CalculateFinalPrice) Execute(input OrderInput) (*OrderOutput, error) {
+	order, err := entitys.NewOrder(input.ID, input.Price, input.Tax)
+	if err != nil {
+		return nil, err
+	}
+	err = order.CalculateFinalPrice()
+	if err != nil {
+		return nil, err
+	}
+	err = c.OrderRepository.Save(order)
+	if err != nil {
+		return nil, err
+	}
+	return &OrderOutput{
+		ID:         order.ID,
+		Price:      order.Price,
+		Tax:        order.Tax,
+		FinalPrice: order.FinalPrice,
+	}, nil
+}
